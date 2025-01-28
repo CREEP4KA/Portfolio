155651cc1e1cded7ee203e062ec0a80d6a303c5a
Naël <nael.creepa@gmail.com>
2025-01-28 20:01:45 +0100

    Delete Base de clients.py

--- Base de clients.py
+++
@@ -1,7 +0,0 @@
-#clients = {"Durand Luc": [{"Adresse":["Résidence les Prés","4 Rue des Pinsons"]},{"CP":"05000"},{"Ville":"Gap"}]}
-
-
-clients={"Nom":"Durand Luc","Adresse":["Résidence les Prés","4 Rue des Pinsons"],"CP":"05000","Ville":"Gap"}
-
-for element in clients.items():
-    print(clients, ":", element)
