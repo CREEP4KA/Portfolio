442e578d30a52847b2c700feb413d65e3a0fe967
Naël <nael.creepa@gmail.com>
2025-05-13 09:48:27 +0200

    Add files via upload

--- IA pierre feuille ciseaux.py
+++ IA pierre feuille ciseaux.py
@@ -16,6 +16,10 @@ import numpy as np
 # proportion gagnée
 
 
+valeur_mystere = input("[coup joué, résultat, nombre jouées, winrate]\n").split(",")
+valeur_mystere = [int(k) for k in valeur_mystere]
+
+
 entrees = np.array( ([3,0,0,0],[3,0,0,0],
                      
                      [1,2,0,0],[3,1,0,0],
@@ -38,12 +42,142 @@ entrees = np.array( ([3,0,0,0],[3,0,0,0],
                      [1,1,0,0],[2,2,0,0],
                      
                      [1,2,0,0],[2,1,0,0],
+                     
+                     
+                     valeur_mystere
 
-
-
-
+                     
                      ) , dtype = float)
 
-0
-fille= 0
-mec= 0
+y_pierre = np.array( ([0],[0],
+                      
+                      [1],[0],
+                      [0],[0],
+                      [1],[0],
+                      [1],[0],
+                      [0],[0],
+                      [1],[0],
+                      [1],[0],
+                      [0],[0],
+                      [1],[0],
+                      
+                      [1],[1],
+                      [1],[1],
+                      [1],[1],
+                      [0],[1],
+                      
+                      [0],[0],
+                      
+                      [1],[0],
+                      
+                      [1],[0]
+                      
+                      ) , dtype = float)
+
+y_feuille = np.array( ([0],[0],
+                      
+                      [0],[0],
+                      [1],[0],
+                      [0],[1],
+                      [0],[1],
+                      [0],[1],
+                      [0],[1],
+                      [0],[0],
+                      [1],[0],
+                      [0],[1],
+                      
+                      [0],[0],
+                      [0],[0],
+                      [0],[0],
+                      [0],[0],
+                      
+                      [0],[1],
+                      
+                      [0],[1],
+                      
+                      [0],[1]
+                      
+                      ) , dtype = float)
+
+y_ciseaux = np.array( ([1],[1],
+                      
+                      [0],[1],
+                      [0],[1],
+                      [0],[0],
+                      [0],[0],
+                      [1],[0],
+                      [0],[0],
+                      [0],[1],
+                      [0],[1],
+                      [0],[0],
+                      
+                      [0],[0],
+                      [0],[0],
+                      [0],[0],
+                      [1],[0],
+                      
+                      [1],[0],
+                      
+                      [0],[0],
+                      
+                      [0],[0]
+                      
+                      ) , dtype = float)
+
+
+entrees = entrees/np.amax(entrees, axis = 0)
+
+X = np.split(entrees,[len(entrees)-2])[0]
+Prediction = np.split(entrees,[len(entrees)-2])[1]
+
+
+class NeuralNetwork:
+    def __init__(self):
+        self.inputSize = 4
+        self.outputSize = 1
+        self.hiddenSize = 5
+        
+        self.W1 = np.random.randn(self.inputSize,self.hiddenSize)
+        self.W2 = np.random.randn(self.hiddenSize,self.outputSize)
+        
+    def forward(self,X):
+        
+        self.z =np.dot(X,self.W1)
+        self.z2 = self.sigmoid(self.z)
+        self.z3 = np.dot(self.z2,self.W2)
+        output = self.sigmoid(self.z3)
+        return output
+    def sigmoid(self, s):
+        return 1/(1+np.exp(-s))
+    
+    def sigmoidPrime(self,s):
+        return s * (1-s)
+    
+    def backward(self,X,y,output) :
+        
+        self.output_error = y - output
+        self.output_delta = self.output_error * self.sigmoidPrime(output)
+        
+        self.z2_error = self.output_delta.dot(self.W2.T)
+        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
+        
+        self.W1 += X.T.dot(self.z2_delta)
+        self.W2 += self.z2.T.dot(self.output_delta)
+        
+    def train(self,X,y):
+        output = self.forward(X)
+        self.backward(X,y,output)
+        
+        
+reseau = NeuralNetwork() 
+output = reseau.forward(X)
+
+for i in range(30):
+    print(str(i) + "\n")
+    print("Valeurs d'entrées : \n" + str(X))
+    print("Sortie attendue : \n" + str(y_pierre) + "\n" + str(y_feuille)+ "\n" + str(y_ciseaux))
+    print("Sortie prédite :\n" + str(np.matrix.round(reseau.forward(X),2)))
+    print()
+    reseau.train(X,y_pierre)
+    reseau.train(X,y_feuille)
+    reseau.train(X,y_ciseaux)
